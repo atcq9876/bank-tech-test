ee1eae08617987806e01d90e797dd51967a6b8db
Andy <ajscottlewis@gmail.com>
2023-01-23 16:02:05 +0000

    Test passes for two consecutivevalid withdrawals followed by printStatement

--- bank.js
+++ bank.js
@@ -1,14 +1,16 @@
 class BankAccount { 
   constructor() {
     this.balance = 0
+    this.statement = 'date || credit || debit || balance'
   }
   
   depositFunds(date, amountDeposited) {
-    this.balance = amountDeposited;
+    this.balance += amountDeposited;
+    this.statement += `\n01/01/2023 || ${amountDeposited.toFixed(2)} || || ${this.balance.toFixed(2)}`;
   }
 
   printStatement() {
-    return `date || credit || debit || balance\n01/01/2023 || ${this.balance.toFixed(2)} || || ${this.balance.toFixed(2)}`
+    return this.statement
   }
 }
 
--- bank.test.js
+++ bank.test.js
@@ -5,20 +5,32 @@ describe('BankAccount', () => {
     const bankAccount = new BankAccount();
     bankAccount.depositFunds('01/01/2023', 100.00);
     
-    expect(bankAccount.printStatement()).toEqual('date || credit || debit || balance\n01/01/2023 || 100.00 || || 100.00')
+    expect(bankAccount.printStatement())
+      .toEqual('date || credit || debit || balance\n01/01/2023 || 100.00 || || 100.00')
   })
 
   it('prints a statement after a different valid deposit', () => {
     const bankAccount = new BankAccount();
     bankAccount.depositFunds('01/01/2023', 200.00);
     
-    expect(bankAccount.printStatement()).toEqual('date || credit || debit || balance\n01/01/2023 || 200.00 || || 200.00')
+    expect(bankAccount.printStatement())
+      .toEqual('date || credit || debit || balance\n01/01/2023 || 200.00 || || 200.00')
   })
 
   it('prints a statement after another different valid deposit', () => {
     const bankAccount = new BankAccount();
     bankAccount.depositFunds('01/01/2023', 250.00);
     
-    expect(bankAccount.printStatement()).toEqual('date || credit || debit || balance\n01/01/2023 || 250.00 || || 250.00')
+    expect(bankAccount.printStatement())
+      .toEqual('date || credit || debit || balance\n01/01/2023 || 250.00 || || 250.00')
+  })
+
+  it('prints a statement after two valid deposits', () => {
+    const bankAccount = new BankAccount();
+    bankAccount.depositFunds('01/01/2023', 250.00);
+    bankAccount.depositFunds('01/01/2023', 500.00);
+    
+    expect(bankAccount.printStatement())
+      .toEqual('date || credit || debit || balance\n01/01/2023 || 250.00 || || 250.00\n01/01/2023 || 500.00 || || 750.00')
   })
 })
