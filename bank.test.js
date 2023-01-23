ac29735be30cca2ee853f0ca9c6d762853a8648e
Andy <ajscottlewis@gmail.com>
2023-01-23 15:48:27 +0000

    Test for a third valid deposit and printStatement passes, change test deposit dates so they are the same

--- bank.test.js
+++ bank.test.js
@@ -10,8 +10,15 @@ describe('BankAccount', () => {
 
   it('prints a statement after a different valid deposit', () => {
     const bankAccount = new BankAccount();
-    bankAccount.depositFunds('02/01/2023', 200.00);
+    bankAccount.depositFunds('01/01/2023', 200.00);
     
-    expect(bankAccount.printStatement()).toEqual('date || credit || debit || balance\n02/01/2023 || 200.00 || || 200.00')
+    expect(bankAccount.printStatement()).toEqual('date || credit || debit || balance\n01/01/2023 || 200.00 || || 200.00')
+  })
+
+  it('prints a statement after another different valid deposit', () => {
+    const bankAccount = new BankAccount();
+    bankAccount.depositFunds('01/01/2023', 250.00);
+    
+    expect(bankAccount.printStatement()).toEqual('date || credit || debit || balance\n01/01/2023 || 250.00 || || 250.00')
   })
 })
