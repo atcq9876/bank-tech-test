904412eea1f2326931e0b7c897e69ad11d82526b
Andy <ajscottlewis@gmail.com>
2023-01-24 12:35:11 +0000

    Refactor: in validation methods, change 'if' statements to 'if, else if' statements to reduce line length

--- bank.js
+++ bank.js
@@ -30,21 +30,32 @@ class BankAccount {
 
   // private methods
   #checkDateIsValid(date) {
-    if (typeof date !== 'string') throw new Error('Please enter the date as a string, in the format DD/MM/YYYY');
     const dateFormat = /^(0?[1-9]|[1-2][0-9]|3[01])[\/](0?[1-9]|1[0-2])/;
-    if (!date.match(dateFormat)) throw new Error('Dates must be in the format DD/MM/YYYY')
-    if (date < this.previousTransactionDate) throw new Error(`You cannot enter a date that is earlier than the previous transaction's`);
+    if (typeof date !== 'string') { 
+      throw new Error('Please enter the date as a string, in the format DD/MM/YYYY');
+    } else if (!date.match(dateFormat)) {
+      throw new Error('Dates must be in the format DD/MM/YYYY')
+    } else if (date < this.previousTransactionDate) {
+      throw new Error(`You cannot enter a date that is earlier than the previous transaction's`);
+    }
   }
 
   #checkAmountDepositedIsValid(amountDeposited) {
-    if (typeof amountDeposited !== 'number') throw new Error('Please enter a number to two decimal places for the amount deposited');
-    if (amountDeposited <= 0) throw new Error('Only positive amounts can be deposited');
+    if (typeof amountDeposited !== 'number') {
+      throw new Error('Please enter a number to two decimal places for the amount deposited');
+    } else if (amountDeposited <= 0) {
+      throw new Error('Only positive amounts can be deposited');
+    }
   }
 
   #checkAmountWithdrawnIsValid(amountWithdrawn) {
-    if (typeof amountWithdrawn !== 'number') throw new Error('Please enter a number to two decimal places for the amount withdrawn');
-    if (amountWithdrawn <= 0) throw new Error('Only positive amounts can be withdrawn');
-    if (this.balance < amountWithdrawn) throw new Error(`Current balance is ${this.balance.toFixed(2)}, withdrawals must not exceed this amount`);
+    if (typeof amountWithdrawn !== 'number') {
+      throw new Error('Please enter a number to two decimal places for the amount withdrawn');
+    } else if (amountWithdrawn <= 0) {
+      throw new Error('Only positive amounts can be withdrawn');
+    } else if (this.balance < amountWithdrawn) {
+      throw new Error(`Current balance is ${this.balance.toFixed(2)}, withdrawals must not exceed this amount`);
+    }
   }
 
   #formatStatement() {
