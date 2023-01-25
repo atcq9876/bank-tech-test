96415eb8caa50ca4f577aceb37bff59863f1c32b
Andy <ajscottlewis@gmail.com>
2023-01-25 18:46:23 +0000

    Test passes when making several transactions and printing more than one statement

--- integration.test.js
+++ integration.test.js
@@ -9,4 +9,19 @@ describe('integration', () => {
 
     expect(bankAccount.getStatement()).toEqual('date || credit || debit || balance\n14/01/2023 || || 500.00 || 2500.00\n13/01/2023 || 2000.00 || || 3000.00\n10/01/2023 || 1000.00 || || 1000.00')
   })
+
+  it('processes deposits, withdrawals and prints statements (another example)', () => {
+    const bankAccount = new BankAccount();
+    bankAccount.depositFunds('20/01/2023', 800.00);
+    bankAccount.withdrawFunds('21/01/2023', 400.00);
+    bankAccount.depositFunds('21/01/2023', 1000.00);
+    bankAccount.withdrawFunds('22/01/2023', 300.00);
+
+    expect(bankAccount.getStatement()).toEqual('date || credit || debit || balance\n22/01/2023 || || 300.00 || 1100.00\n21/01/2023 || 1000.00 || || 1400.00\n21/01/2023 || || 400.00 || 400.00\n20/01/2023 || 800.00 || || 800.00')
+
+    bankAccount.withdrawFunds('24/01/2023', 100.00);
+    bankAccount.depositFunds('25/01/2023', 200.00);
+
+    expect(bankAccount.getStatement()).toEqual('date || credit || debit || balance\n25/01/2023 || 200.00 || || 1200.00\n24/01/2023 || || 100.00 || 1000.00\n22/01/2023 || || 300.00 || 1100.00\n21/01/2023 || 1000.00 || || 1400.00\n21/01/2023 || || 400.00 || 400.00\n20/01/2023 || 800.00 || || 800.00')
+  })
 })
