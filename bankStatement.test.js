394744b41d7126b5e0c3f1ff5e25f60afc331afe
Andy <ajscottlewis@gmail.com>
2023-01-25 14:49:39 +0000

    Correct typo

--- bankStatement.test.js
+++ bankStatement.test.js
@@ -26,21 +26,21 @@ describe('BankStatement', () => {
     expect(bankStatement.printStatement()).toEqual('Current balance is 0, no transactions have taken place');
   })
 
-  it('prints a statment for one deposit', () => {
+  it('prints a statement for one deposit', () => {
     bankAccount.transactions = '\n01/01/2023 || 100.00 || || 100.00';
     const bankStatement = new BankStatement(bankAccount);
     
     expect(bankStatement.printStatement()).toEqual('date || credit || debit || balance\n01/01/2023 || 100.00 || || 100.00');
   })
 
-  it('prints a statment for a different deposit', () => {
+  it('prints a statement for a different deposit', () => {
     bankAccount.transactions = '\n01/01/2023 || 200.00 || || 200.00';
     const bankStatement = new BankStatement(bankAccount);
     
     expect(bankStatement.printStatement()).toEqual('date || credit || debit || balance\n01/01/2023 || 200.00 || || 200.00');
   })
 
-  it('prints a statment for two deposits', () => {
+  it('prints a statement for two deposits', () => {
     bankAccount.transactions = '\n01/01/2023 || 500.00 || || 750.00\n01/01/2023 || 250.00 || || 250.00';
     const bankStatement = new BankStatement(bankAccount);
     
