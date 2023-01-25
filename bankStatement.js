209e79bbfd6e2b24c8acc4fb52578755fbc6c0ad
Andy <ajscottlewis@gmail.com>
2023-01-25 14:55:42 +0000

    Refactor: move statement formatting logic to new private method #formatStatement; add instance variable this.statement

--- bankStatement.js
+++ bankStatement.js
@@ -4,14 +4,20 @@ class BankStatement {
   constructor(bankAccount) {
     if (!(bankAccount instanceof BankAccount)) throw new Error('Only an instance of BankAccount can be passed to BankStatement')
     this.transactions = bankAccount.transactions;
+    this.statement = ''
   }
   
   printStatement() {
     if (this.transactions === '') {
       return 'Current balance is 0, no transactions have taken place';
     } else {
-      return 'date || credit || debit || balance' + this.transactions;
+      this.#formatStatement();
+      return this.statement;
     }
+  }
+
+  #formatStatement() {
+    this.statement = 'date || credit || debit || balance' + this.transactions;
   }
 }
 
