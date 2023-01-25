14c1f4cfd6f37cc8536bbbb5d9520eada8807fb7
Andy <ajscottlewis@gmail.com>
2023-01-25 13:25:18 +0000

    Refactor tests: remove beforeEach statement, pass an empty string to first test (printing statement after 0 transactions

--- bankStatement.test.js
+++ bankStatement.test.js
@@ -1,13 +1,10 @@
 const BankStatement = require('./bankStatement');
 
 describe('BankStatement', () => {
-  let bankStatement;
-
-  beforeEach(() => {
-    bankStatement = new BankStatement();
-  })
-
+  
   it('prints a message saying the balance is 0 if no transactions have taken place', () => {
+    const bankStatement = new BankStatement('')
+    
     expect(bankStatement.printStatement()).toEqual('Current balance is 0, no transactions have taken place');
   })
 })
