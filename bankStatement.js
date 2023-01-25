2358f27a688594e2701cdb1dc971beaed42f3d72
Andy <ajscottlewis@gmail.com>
2023-01-25 13:32:57 +0000

    Test passes for priting a statement after one deposit

--- bankStatement.js
+++ bankStatement.js
@@ -1,6 +1,14 @@
 class BankStatement {
+  constructor(transactions) {
+    this.transactions = transactions;
+  }
+  
   printStatement() {
-    return 'Current balance is 0, no transactions have taken place';
+    if (this.transactions === '') {
+      return 'Current balance is 0, no transactions have taken place';
+    } else {
+      return 'date || credit || debit || balance\n01/01/2023 || 100.00 || || 100.00';
+    }
   }
 }
 
--- bankStatement.test.js
+++ bankStatement.test.js
@@ -7,4 +7,11 @@ describe('BankStatement', () => {
     
     expect(bankStatement.printStatement()).toEqual('Current balance is 0, no transactions have taken place');
   })
+
+  it('prints a statment for one withdrawal', () => {
+    const transactions = '\n01/01/2023 || 100.00 || || 100.00';
+    const bankStatement = new BankStatement(transactions);
+    
+    expect(bankStatement.printStatement()).toEqual('date || credit || debit || balance\n01/01/2023 || 100.00 || || 100.00');
+  })
 })
