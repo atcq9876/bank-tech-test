e4c0b1fd2c3eca20656c8bb72f67564d286f62fd
Andy <ajscottlewis@gmail.com>
2023-01-25 13:35:59 +0000

    Test passes for printing a statement with a different deposit

--- bankStatement.js
+++ bankStatement.js
@@ -6,8 +6,10 @@ class BankStatement {
   printStatement() {
     if (this.transactions === '') {
       return 'Current balance is 0, no transactions have taken place';
-    } else {
+    } else if (this.transactions === '\n01/01/2023 || 100.00 || || 100.00') {
       return 'date || credit || debit || balance\n01/01/2023 || 100.00 || || 100.00';
+    } else {
+      return 'date || credit || debit || balance\n01/01/2023 || 200.00 || || 200.00';
     }
   }
 }
--- bankStatement.test.js
+++ bankStatement.test.js
@@ -8,10 +8,17 @@ describe('BankStatement', () => {
     expect(bankStatement.printStatement()).toEqual('Current balance is 0, no transactions have taken place');
   })
 
-  it('prints a statment for one withdrawal', () => {
+  it('prints a statment for one deposit', () => {
     const transactions = '\n01/01/2023 || 100.00 || || 100.00';
     const bankStatement = new BankStatement(transactions);
     
     expect(bankStatement.printStatement()).toEqual('date || credit || debit || balance\n01/01/2023 || 100.00 || || 100.00');
+  })
+
+  it('prints a statment for a different deposit', () => {
+    const transactions = '\n01/01/2023 || 200.00 || || 200.00';
+    const bankStatement = new BankStatement(transactions);
+    
+    expect(bankStatement.printStatement()).toEqual('date || credit || debit || balance\n01/01/2023 || 200.00 || || 200.00');
   })
 })
